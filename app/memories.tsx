eb1f3b46f457a6a64a7a8240095884327a71c8df
Bruno Garcia <brunowilliang@MacBook-Pro-de-Bruno-Garcia.local>
2023-05-20 18:25:35 +0100

    first commit

---
+++ app/memories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+// import { Container } from './styles';
+
+const Memories = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="text-white">Memories</Text>
+    </View>
+  )
+}
+
+export default Memories
